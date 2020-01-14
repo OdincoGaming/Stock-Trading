9d8f3ea405d51712ed9a8eb30359385db40c294d
Freddy Lyle <OdincoGaming@gmail.com>
2020-01-14 09:10:45 -0600

    bot functionality
    added functions for most of the stuff alpaca offers, in the future might add short sales, watchlists, and pre-market/afterhours capability (theres special rules for those trades so wouldnt want to break any rules while were learning)

--- web_example/bot.py
+++ web_example/bot.py
@@ -1,5 +1,7 @@
 import alpaca_trade_api as tradeapi
 import time
+import csv
+import os
 
 def GetMarketHours():
     is_market_open = api.get_clock()
@@ -37,6 +39,100 @@ def Algo_4(symbol, days_to_hold):
     #focused
     #hold strategy, buy when cheap. sell after set period of time
     return 0
+
+#api call to submit a purchase order
+def BuyAtMarket(symbol, qty, tif,):
+    api.submit_order(
+    symbol=symbol,
+    qty=qty,
+    side='buy',
+    type='market',
+    time_in_force=tif
+)
+
+def BuyAtLimit(symbol, qty, tif, limit_price):
+    api.submit_order(
+    symbol=symbol,
+    qty=qty,
+    side='buy',
+    type='limit',
+    limit_price=limit_price
+    time_in_force=tif
+)
+
+def BuyAtStopLimit(symbol, qty, tif, limit_price, stop_price):
+    api.submit_order(
+    symbol=symbol,
+    qty=qty,
+    side='buy',
+    type='stop_limit',
+    limit_price=limit_price
+    stop_price=stop_price
+    time_in_force=tif
+)
+
+#api call to submit a sell order
+def SellAtMarket(symbol, qty, tif):
+    api.submit_order(
+    symbol=symbol,
+    qty=qty,
+    side='sell',
+    type='market',
+    time_in_force=tif,
+)
+
+def SellAtLimit(symbol, qty, tif, limit_price):
+    api.submit_order(
+    symbol=symbol,
+    qty=qty,
+    side='sell',
+    type='limit',
+    limit_price=limit_price
+    time_in_force=tif
+)
+
+def SellAtStopLimit(symbol, qty, tif, limit_price, stop_price):
+    api.submit_order(
+    symbol=symbol,
+    qty=qty,
+    side='sell',
+    type='stop_limit',
+    limit_price=limit_price
+    stop_price=stop_price
+    time_in_force=tif
+)
+
+#will be replacing this with a seperate API as alpacas data is kind of ehh
+def GetData(symbol, enoch, limit):
+    #a list with a list which is a lists of lists (I assume you can send in multiple symbols at once to get a list of lists that are lists of lists)
+    #to get data on multiple symbols in one call send symbols as string seperated by , "AAPL, NFLX"
+    #enoch is time period, day minute month year
+    #limit is how many enochs, so enoch='minute', limit=1 would give the most recent data
+    barset = api.get_barset(symbol, enoch, limit=limit)
+
+    #top level list in barset
+    bars = barset[symbol]
+
+    filename = symbol + '.csv'
+    path = os.getcwd() + "\\data\\csv\\" + filename
+
+    try:
+        with open(path, 'w+', newline='') as file:
+            i = 0
+            fieldnames = ['date', '1. open', '2. high', '3. low', '4. close', '5. volume']
+            writer = csv.DictWriter(file, fieldnames=fieldnames)
+            writer.writeheader()
+            while i < limit:
+                t = bars[i].t
+                o = bars[i].o
+                h = bars[i].h
+                l = bars[i].l
+                c = bars[i].c
+                v = bars[i].v
+                writer.writerow({'date':t,'1. open':o,'2. high':h,'3. low':l,'4. close':c,'5. volume':v})
+                i += 1
+    except:
+        print("not enough data for " + symbol)
 
 def Main(is_open):
     #continue to trade until market is closed
