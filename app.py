fc463266adbd88513a61650e404b50c6314f22ca
Freddy Lyle <OdincoGaming@gmail.com>
2020-01-20 00:44:08 -0600

    added CORS

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 from flask import Flask, render_template, request
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+#just add @cross_origin() decorator beneath any route and-
+#above any definition that requires it
 
 @app.route('/')
+#@cross_origin()
 def index():
     return render_template('index.html')
 
